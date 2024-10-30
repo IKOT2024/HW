f60bcf4d183ac222efb81a9328e131b6aa853f12
IKOT2024 <ikot05880@gmail.com>
2024-10-30 21:02:29 +0300

    Revert "Проверь те пожалуйста дз. Без верстки. Только задачи по js"

--- skript.js.2.0.js
+++ skript.js.2.0.js
@@ -1,144 +1,121 @@
-// Урок 2.5
+// // Урок 2.5
 
-//1
-function minNumber(a, b) {
-      if (a <= b) {
-            return a;
-      } else {
-            return b;
-      }
-}
+// //1
+// function minNumber(a, b) {
+//       if (a <= b) {
+//             return a;
+//       } else {
+//             return b;
+//       }
+// }
 
-console.log("1 число :", minNumber(4, 8));
-console.log("2 число :", minNumber(6, 6));
-console.log("3 число :", minNumber(110, 170));
-
-
-
-//2
-function retyrnNumber(n) {
-      if (n % 2 == 0) {
-            return 'Число четное'
-      } else {
-            return 'Число нечетное'
-      }
-}
-
-console.log(retyrnNumber(78));
+// console.log("1 число :", minNumber(4, 8));
+// console.log("2 число :", minNumber(6, 6));
+// console.log("3 число :", minNumber(110, 170));
 
 
 
-//3
-let acceptNumber = (x) => x * x;
+// //2
+// function retyrnNumber(n) {
+//       if (n % 2 == 0) {
+//             return 'Число четное'
+//       } else {
+//             return 'Число нечетное'
+//       }
+// }
 
-let x = 3;
-let accept = acceptNumber(x);
-
-console.log(`Квадрат числа ${x} равен ${accept}`);
-
-let doubleSquare = accept * 2;
-console.log(`Удвоенный квадрат числа ${x} равен ${doubleSquare}`);
-
-
-4
-function userAge() {
-
-    let age = prompt('Сколько вам лет?');
-    
-    if (isNaN(age)) {
-        console.log('Введенное значение не является числом')
-    }
-    else if (age < 0) {
-        console.log('Вы ввели неправильное значение');
-    } else if (age <= 12) {
-        console.log('привет друг');
-    } else if (age >= 13) {
-        console.log('Добро пожаловать');
-    }
-
-}
-
-userAge();
-
-
-function multiplyIfNumbers(a, b) {
-
-    let numerA = Number(a);
-    let numerB = Number(b);
-
-    
-    if (isNaN(numerA) || isNaN(numerB)) {
-        return 'Одно или оба значения не являются числом';
-    }
-
-    
-    return numerA * numerB;
-}
-
-console.log(multiplyIfNumbers(23, 35)); 
-console.log(multiplyIfNumbers("3", 945)); 
-console.log(multiplyIfNumbers("sfasd", 11)); 
-console.log(multiplyIfNumbers(81, "ergeg")); 
-console.log(multiplyIfNumbers("43", "56")); 
-
-//6
-function enterNumber() {
-      let p = prompt('введите пожалуйста число');
-
-      if (isNaN(p)) {
-
-            return ('Переданный параметр не является числом');
-      }
-      else {
-            return p * p;
-
-      }
-}
-
-console.log("1 значение :", enterNumber());
-console.log("2 значение :", enterNumber());
+// console.log(retyrnNumber(78));
 
 
 
-//7
-let circle1 = {
-      radius: 9,
-      getArea() {
-            return Math.PI * Math.pow(this.radius, 2)
-      },
+// //3
+// let acceptNumber = (x) => x * x;
 
-      getPerimeter() {
-            return 2 * Math.PI * this.radius;
-      }
-};
+// let x = 3;
+// let accept = acceptNumber(x);
 
-let circle2 = {
-      radius: 23,
+// console.log(`Квадрат числа ${x} равен ${accept}`);
 
-      getArea() {
-            return Math.PI * Math.pow(this.radius, 2);
-      },
-
-        getPerimeter() {
-            return 2 * Math.PI * this.radius;
-      }
-};
-
-console.log("Circle1 Area:", circle1.getArea());
-console.log("Circle1 Perimeter:", circle1.getPerimeter());
-console.log("Circle2 Area:", circle2.getArea());
-console.log("Circle2 Perimeter:", circle2.getPerimeter());
+// let doubleSquare = accept * 2;
+// console.log(`Удвоенный квадрат числа ${x} равен ${doubleSquare}`);
 
 
+// //4
+// function userAge() {
 
-const numbers = [2, 4, 5, 6, 8, 9, 11];
-numbers.forEach((el, index, arr) => {
-    console.log(`${index} : ${el}, массив : ${arr}`);
-});
+//       let ageUser = prompt('Сколько вам лет?');
+
+//       if (ageUser < 0) {
+//             console.log("Вы ввели неправильное значение");
+//       } else if (ageUser >= 0 && ageUser <= 12) {
+//             console.log("привет друг");
+//       } else if (ageUser >= 13) {
+//             console.log("Добро пожаловать");
+//       }
+// }
+// userAge();
+
+// //5
+// function typeСonversion(x) {
+//       if (isNaN(x)) {
+//             return ('Одно или оба значения не являются числом');
+//       }
+//       else {
+//             return x * x
+//       }
+
+// }
+
+// console.log("1 значение :", typeСonversion(112));
+// console.log("2 значение :", typeСonversion('abc'));
 
 
 
+// //6
+// function enterNumber() {
+//       let p = prompt('введите пожалуйста число');
+
+//       if (isNaN(p)) {
+
+//             return ('Переданный параметр не является числом');
+//       }
+//       else {
+//             return p * p;
+
+//       }
+// }
+
+// console.log("1 значение :", enterNumber());
+// console.log("2 значение :", enterNumber());
 
 
 
+// //7
+// let circle1 = {
+//       radius: 9,
+//       getArea() {
+//             return Math.PI * Math.pow(this.radius, 2)
+//       },
+
+//       getPerimeter() {
+//             return 2 * Math.PI * this.radius;
+//       }
+// };
+
+// let circle2 = {
+//       radius: 23,
+
+//       getArea() {
+//             return Math.PI * Math.pow(this.radius, 2);
+//       },
+
+//         getPerimeter() {
+//             return 2 * Math.PI * this.radius;
+//       }
+// };
+
+// console.log("Circle1 Area:", circle1.getArea());
+// console.log("Circle1 Perimeter:", circle1.getPerimeter());
+// console.log("Circle2 Area:", circle2.getArea());
+// console.log("Circle2 Perimeter:", circle2.getPerimeter()); 
 
